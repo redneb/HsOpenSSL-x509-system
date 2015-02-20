41092e8edb4b68b29a5bf5bd8a16215450034105
Marios Titas <redneb@gmx.com>
2015-02-20 03:05:53 +0000

    fix typo

--- OpenSSL/X509/SystemStore.hs
+++ OpenSSL/X509/SystemStore.hs
@@ -17,7 +17,7 @@ import qualified OpenSSL.X509.SystemStore.Unix as S
 -- given @openssl@ context. Note that this does not automatically enable
 -- peer certificate verification. You also need to call
 -- 'OpenSSL.Session.contextSetVerificationMode' and check manually if the
--- hostname matches with the one specified in the certificate.
+-- hostname matches the one specified in the certificate.
 contextLoadSystemCerts :: SSLContext -> IO () 
 contextLoadSystemCerts = S.contextLoadSystemCerts
 {-# INLINE contextLoadSystemCerts #-}
