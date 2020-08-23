22e953fe54f874408607e73131ad86c87ebf1474
Marios Titas <redneb@gmx.com>
2020-08-23 20:09:21 +0300

    rephrase warning about cert validation as it only applies to older versions of OpenSSL

--- OpenSSL/X509/SystemStore.hs
+++ OpenSSL/X509/SystemStore.hs
@@ -14,10 +14,11 @@ import qualified OpenSSL.X509.SystemStore.Unix as S
 #endif
 
 -- | Add the certificates from the system-wide certificate store to the
--- given @openssl@ context. Note that
--- __this does not automatically enable peer certificate verification__.
--- You also need to call 'OpenSSL.Session.contextSetVerificationMode' and
--- __check manually if the hostname matches__ the one specified in the
+-- given @openssl@ context. Note that in __older versions of OpenSSL__
+-- (namely <1.1.0), this does not automatically enable peer certificate
+-- verification. In that case,
+-- you also need to call 'OpenSSL.Session.contextSetVerificationMode' and
+-- check manually if the hostname matches the one specified in the
 -- certificate. You can find information about how to do the latter
 -- <https://github.com/iSECPartners/ssl-conservatory/blob/master/openssl/everything-you-wanted-to-know-about-openssl.pdf here>.
 contextLoadSystemCerts :: SSLContext -> IO () 
